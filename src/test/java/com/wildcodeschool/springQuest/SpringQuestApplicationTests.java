41b55e6060c4a7e2ff0e0054ea07488b9dc73c18
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-11-14 11:14:52 +0100

    quest01

---
+++ src/test/java/com/wildcodeschool/springQuest/SpringQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.springQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
